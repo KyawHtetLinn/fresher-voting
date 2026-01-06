f9f661229a2a4306598f9dad9b101589ffe95beb
KyawHtetLinn <kyawhtetzine71@gmail.com>
2026-01-06 22:16:55 +0630

    Fix bug

--- backend/voting/admin.py
+++ backend/voting/admin.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
 from .models import Candidate, VoteTransaction
 
+
 @admin.register(Candidate)
 class CandidateAdmin(admin.ModelAdmin):
-    list_display = ('candidate_number', 'name', 'category', 'vote_count')
-    list_filter = ('category',)
-    search_fields = ('name',)
-    actions = ['reset_votes_to_zero'] # <--- Add the action button
+    list_display = ("candidate_number", "name", "category", "vote_count")
+    list_filter = ("category",)
+    search_fields = ("name",)
+    actions = ["reset_votes_to_zero"]
 
     # Define the action function
-    @admin.action(description='Reset selected candidates to 0 votes')
+    @admin.action(description="Reset selected candidates to 0 votes")
     def reset_votes_to_zero(self, request, queryset):
         rows_updated = queryset.update(vote_count=0)
-        self.message_user(request, f"{rows_updated} candidates have been reset to 0 votes.")
-        
-        
+        self.message_user(
+            request, f"{rows_updated} candidates have been reset to 0 votes."
+        )
+
+
 @admin.register(VoteTransaction)
 class VoteTransactionAdmin(admin.ModelAdmin):
-    list_display = ('device_id', 'candidate', 'timestamp')
-    readonly_fields = ('device_id', 'candidate', 'timestamp', 'ip_address')
+    list_display = ("device_id", "candidate", "timestamp")
+    readonly_fields = ("device_id", "candidate", "timestamp", "ip_address")
